47462084c6d0ef034891f87b8e6a49e44c3de8fb
Nebrethar <nebrethar@gmail.com>
2018-07-09 21:37:56 -0500

    Fixes and icon changes

--- data/popup/data.js
+++ data/popup/data.js
@@ -1,8 +1,8 @@
-
+/* function MonitorNetowkr copied from https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/webRequest/StreamFilter/ondata */
 function monitorNetwork() {
 	let filter = browser.webRequest.filterResponseData(details.requestID);
 	let decoder = new TextDecoder("utf-8");
-	
+
     filter.ondata = event => {
       filter.write(event.data);
 	  let str = decoder.decode(event.data, {stream: true});
